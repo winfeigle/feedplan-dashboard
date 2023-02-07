d9c0ac60f29aa98e5101d02a36c05aad8512f4ec
Win Feigle <win.feigle@gmail.com>
2023-02-07 16:54:40 -0700

    Import redux resources/folders

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from '@redux-devtools/extension';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider>
+    {/* <React.StrictMode> */}
+      <App />
+    {/* </React.StrictMode> */}
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ client/src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+
+export default rootReducer = combineReducers({
+    // Example...
+    potato: potatoReducer, 
+    tomato: tomatoReducer,
+})
