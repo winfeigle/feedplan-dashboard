8fc951368f31aa09d1c86ce1338c1b727221914e
Win Feigle <win.feigle@gmail.com>
2023-02-10 13:12:02 -0700

    Add Login component

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,17 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { TextField, Button } from '@mui/material';
+
 
 function LoginForm(){
+    const [ loginData, setLoginData ] = useState({
+        username: "",
+        password: ""
+    })
+
+    const onFormChange = (e) => {
+        setLoginData({...loginData, [e.target.name]: e.target.value});
+    }
+
+    const onFormSubmit = (e) => {
+        e.preventDefault()
+        console.log(loginData)
+        setLoginData({username: "",password: ""})
+    }
+
+
     return(
         <div>
-            <form>
-                <label> Username
-                    <input
-                        placeholder="username"
-                        name="username"
-                        // value={username}
-                        // onChange={handleFormChange}
-                        />
-                </label>
+            <form onSubmit={onFormSubmit}>
+                <TextField
+                    id="outlined-required"
+                    label="username"
+                    type="username"
+                    // type={ error ? "Display the error here..." : null }
+                    name="username"
+                    value={loginData.username}
+                    onChange={(e) => onFormChange(e)}
+                    />
+                <TextField
+                    id="outlined-password-input"
+                    label="password"
+                    type="password"
+                    // type={ error ? "Display the error here..." : null }
+                    name="password"
+                    value={loginData.password}
+                    onChange={(e) => onFormChange(e)}
+                    />
+                <Button 
+                    variant="contained" 
+                    type="submit"
+                    disableElevation
+                    >Log in</Button>
             </form>
         </div>
     );
