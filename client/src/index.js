c938c1eea17b037a464366fb14568751861a9953
Win Feigle <win.feigle@gmail.com>
2023-02-10 11:16:22 -0700

    Reset folders without Redux

--- client/src/index.js
+++ client/src/index.js
@@ -3,17 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from '@redux-devtools/extension';
 
 ReactDOM.render(
-  <Provider>
-    {/* <React.StrictMode> */}
-      <App />
-    {/* </React.StrictMode> */}
-  </Provider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
--- client/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-
-export default rootReducer = combineReducers({
-    // Example...
-    potato: potatoReducer, 
-    tomato: tomatoReducer,
-})
