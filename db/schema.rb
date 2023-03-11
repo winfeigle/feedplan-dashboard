9683d3d9f65bddf69cf5ed5ba8e2f4886924ffdf
Win Feigle <win.feigle@gmail.com>
2023-03-11 13:33:17 -0700

    Update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -23,38 +23,5 @@ ActiveRecord::Schema.define(version: 2023_02_10_213447) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "reviews", force: :cascade do |t|
-    t.string "title"
-    t.text "comment"
-    t.bigint "user_id", null: false
-    t.bigint "space_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["space_id"], name: "index_reviews_on_space_id"
-    t.index ["user_id"], name: "index_reviews_on_user_id"
-  end
-
-  create_table "spaces", force: :cascade do |t|
-    t.string "name"
-    t.string "location"
-    t.text "description"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "image_url"
-    t.float "rating"
-    t.integer "price"
-    t.index ["user_id"], name: "index_spaces_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  add_foreign_key "reviews", "spaces"
-  add_foreign_key "reviews", "users"
-  add_foreign_key "spaces", "users"
+  
 end
