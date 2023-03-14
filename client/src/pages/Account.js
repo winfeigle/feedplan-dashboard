e08f993761155889220d8b6c03164bb00b910e31
Win Feigle <win.feigle@gmail.com>
2023-03-14 09:26:34 -0600

    Add account styles to tab

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import {UserContext} from "../context/UserContext";
+
 import Button from "react-bootstrap/esm/Button";
 
 export default function Account(){
@@ -7,10 +8,16 @@ export default function Account(){
 
     return(
         <div id="static-account-container">
-            <Button variant="feedplan-purple" size="lg" style={{ display: "flex", alignContent: "center" }}>
-                {/* <img id="profile-icon" src={profile} /> */}
-                <div id="profile-icon">{user.username[0].toUpperCase()}</div>
-                <div>＋</div>
+            <Button 
+                variant="feedplan-purple" 
+                size="lg" 
+                style={{ 
+                    display: "flex", 
+                    alignContent: "center" }}
+                >
+                <div id="profile-icon">{user.username[0].toUpperCase()}</div> 
+                    <div className="horizontal-account-break"></div>
+                <div id="profile-plus-icon">＋</div>
             </Button>
         </div>
     );
