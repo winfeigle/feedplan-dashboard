b809df71b0e93001f213e1ef131b273af2887256
Win Feigle <win.feigle@gmail.com>
2023-03-21 06:58:11 -0600

    Remove unnecessary routes from restaurants_meal_plan

--- app/controllers/restaurants_meal_plan_controller.rb
+++ app/controllers/restaurants_meal_plan_controller.rb
@@ -1,26 +1,4 @@
 class RestaurantsMealPlanController < ApplicationController
-    skip_before_action :authorize, only: [ :index, :show, :create ]
-
-    def index
-        render json: RestaurantsMealPlan.all, status: :ok
-    end
-
-    def show
-        meal_plan_assignments = RestaurantsMealPlan.find_by!(meal_plan_id: params[:meal_plan_id])
-
-        render json: meal_plan_assignments, status: :ok,
-        serializer: MealPlanAssignmentsSerializer
-    end
-
-    def create
-        assigned_meal_plan = RestaurantsMealPlan.create!(assignment_params)
-        render json: assigned_meal_plan, status: :created
-    end
-
-    private
-
-    def assignment_params
-        params.permit(:restaurant_id, :meal_plan_id)
-    end
+    
 
 end
