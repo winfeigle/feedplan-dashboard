1b4ba446f0d7438357389bce45cf9fb28b6e3444
Win Feigle <win.feigle@gmail.com>
2023-03-21 17:04:11 -0600

    Render menu cards to page

--- client/src/components/MenuItem.js
+++ client/src/components/MenuItem.js
@@ -1,8 +1,22 @@
 import React from "react";
 
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
 export default function MenuItem({ menu_item }){
-    
+
     return(
-        <p>{menu_item.name}</p>
+        <Card 
+            style={{ 
+                width: '14rem',
+                marginRight: '1rem'
+                }}>
+            <Card.Img variant="top" src={menu_item.image_url} />
+            <Card.Body>
+                <Card.Title>{menu_item.name}</Card.Title>
+                <Card.Text>{menu_item.description.slice(0, 50) + '...'}</Card.Text>
+                <Button variant="outline-feedplan-purple">Go somewhere</Button>
+            </Card.Body>
+        </Card>
     );
 }
